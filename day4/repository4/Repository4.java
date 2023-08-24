796db73cf3ac26e5e31da335e377a82dcc18f3f6
SUNIL K <108710362+sunil19042004@users.noreply.github.com>
2023-08-24 15:53:43 +0530

    Update Repository4.java

--- day4/repository4/Repository4.java
+++ day4/repository4/Repository4.java
@@ -1,10 +1,9 @@
 package com.sunil.RestapiD.day4.repository4;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.sunil.RestapiD.day4.model4.Model4;
-
-
-public interface Repository4 extends JpaRepository<Model4, Integer>{
+@Repository
+public interface Repository4 extends JpaRepository<Model4,Integer>{
 
 }
