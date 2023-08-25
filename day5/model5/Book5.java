1c74ca3b73fd711623f04661718c5ffef48edd91
SUNIL K <108710362+sunil19042004@users.noreply.github.com>
2023-08-25 14:36:55 +0530

    Update Book5.java

--- day5/model5/Book5.java
+++ day5/model5/Book5.java
@@ -1,12 +1,15 @@
 package com.sunil.RestapiD.day5.model5;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 @Entity
 @Table(name="book")
 public class Book5 {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String bookName;
 	private float price;
